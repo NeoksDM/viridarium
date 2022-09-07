5f0e3bf7e24a4b4ab7142f5ff63301b71a9c3c57
Raleth <dalonvia@gmail.com>
2022-09-07 16:25:41 -0500

    desorden XD y pagina de inicio

---
+++ viravirum.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="es" dir="ltr">
+<head>
+<meta charset="utf-8">
+
+<title>Viridarium</title>
+
+<link rel="stylesheet" href="estilo.css">
+
+</head>
+
+
+<body>
+    <h1>Viridarium</h1>
+
+    <p>
+
+    <a href="calculadora.php" class="btn-neon">
+
+    <span >! JUGAR ¡</span>
+
+
+
+    </a>
+
+    <br>
+
+    </p>
+</body>
+
+</html>
