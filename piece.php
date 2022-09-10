e9159d35745d0497a2d38e9851727418cbbab0f9
NeoksDM <NeoksPP@proton.me>
2022-09-10 10:59:28 -0500

    funciones iniciales

--- piece.php
+++ piece.php
@@ -1,19 +1,41 @@
 <?php 
-echo 'coño de la puta madre'.'<br/>';
+echo 'chucha madre'.'<br/>';
 
-for ($i=1; $i <=75 ; $i++) { 
-    for($j=1; $j <=75 ; $j++){
-
-        $n=rand(0,2);
-        $plano[$i][$j] = $n;
-        echo $plano[$i][$j].'    ';
-    }
-    echo '<br/>';
+for ($i=0; $i <10; $i++) { 
+    $vector[$i]=rand(1,9);
+    echo $vector[$i].'<br/>';
 }
- $x=35;
- $y=35;
- $pos_jugador = array($x,$y);
-$n=rand(0,1);
+echo var_dump($vector);
+$longitud = 9;
+/*
+function Sucesion_base($indice) {
+    global $longitud;
+    for($i=0; $i<$longitud; $i++) {
+        $sucesión[$i] = $i+1;
+    }
+    return $sucesión[$indice];
+}
 
+
+
+
+function Desfase($indice) {
+global $longitud;
+$desfase = floor($indice/$longitud);
+return $desfase;
+
+function Adicíon($a, $b) {
+    global $longitud;   
+    $adición_inicial = Sucesion_base($a) + 
+Sucesion_base($longitud, $b);
+    if($adición_inicial<$longitud) {
+        $adicion_final = $adición_inicial;
+    } else {
+        $adicion_final = $adición_inicial - Desfase()
+    }
+}
+
+function Producto
+*/
 ?>
 
